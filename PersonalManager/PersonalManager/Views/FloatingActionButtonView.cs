49b65255684cfc4fed2355130c101abf2335ce96
Vojtěch Mádr <madrvojtech@outlook.com>
2018-06-14 15:26:51 +0200

    Some fixes

--- PersonalManager/PersonalManager/Views/FloatingActionButtonView.cs
+++ PersonalManager/PersonalManager/Views/FloatingActionButtonView.cs
@@ -1,8 +1,4 @@
-﻿using PersonalManager.Enums;
-using System;
-using System.Collections.Generic;
-using System.Text;
-using Xamarin.Forms;
+﻿using Xamarin.Forms;
 
 namespace PersonalManager.Views
 {
